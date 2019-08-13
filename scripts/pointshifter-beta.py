1def5959c1fa7f2abd2b105c6f30599a501bdb21
Agyei <hello@agyei.design>
2019-08-13 08:28:55 -0400

    updated pointshifter for xOpq

--- scripts/pointshifter-beta.py
+++ scripts/pointshifter-beta.py
@@ -1,6 +1,28 @@
 glyph=CurrentGlyph()
 
-glyph.prepareUndo()
+def xOPQ(stemAdjustment):
 
-for point in glyph.contours[0].selection:
-    point.moveBy((-20,0))
+    glyph.prepareUndo()
+    
+    for i in range(len(glyph.contours)):
+        
+        noShiftingRight=True
+        noShiftingLeft=True
+        
+        for point in glyph.contours[i].selection:
+            if point.x > glyph.width/2:
+                point.moveBy((stemAdjustment,0))
+                noShiftingRight=False
+                print("right contour points moved by "+str(stemAdjustment)+ " points")
+            if point.x < glyph.width/2:
+                point.moveBy((-stemAdjustment,0))
+                noShiftingLeft=False
+
+        if noShiftingRight is False:
+            glyph.rightMargin+=stemAdjustment
+        if noShiftingLeft is False:
+            glyph.leftMargin+=stemAdjustment
+
+xOPQ(130)
+
+print(glyph.contours)
