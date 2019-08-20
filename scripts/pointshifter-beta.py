3259313661c17d099250fe717c23dccdeb1fb5ba
Agyei <hello@agyei.design>
2019-08-20 01:12:07 -0400

    first xopq yopq trials. next up: xtra

--- scripts/pointshifter-beta.py
+++ scripts/pointshifter-beta.py
@@ -1,14 +1,17 @@
+f=CurrentFont()
 glyph=CurrentGlyph()
 
 def xOPQ(stemAdjustment):
-
+    
     glyph.prepareUndo()
     
     for i in range(len(glyph.contours)):
         
+        glyph.prepareUndo()
+
         noShiftingRight=True
         noShiftingLeft=True
-        
+ 
         for point in glyph.contours[i].selection:
             if point.x > glyph.width/2:
                 point.moveBy((stemAdjustment,0))
@@ -17,12 +20,33 @@ def xOPQ(stemAdjustment):
             if point.x < glyph.width/2:
                 point.moveBy((-stemAdjustment,0))
                 noShiftingLeft=False
+                print("left contour points moved by "+str(stemAdjustment)+ " points")
 
         if noShiftingRight is False:
             glyph.rightMargin+=stemAdjustment
         if noShiftingLeft is False:
             glyph.leftMargin+=stemAdjustment
+            
+    glyph.performUndo()
 
-xOPQ(130)
+# xOPQ(130)
 
-print(glyph.contours)
+def yOPQ(stemAdjustment):
+    
+    glyph.prepareUndo()
+    
+    for i in range(len(glyph.contours)):
+        
+        glyph.prepareUndo()
+ 
+        for point in glyph.contours[i].selection:
+            if point.y < f.info.capHeight/2:
+                point.moveBy((0,stemAdjustment))
+                print("top contour points moved by "+str(stemAdjustment)+ " points")
+            elif point.y > f.info.capHeight/2:
+                point.moveBy((0,-stemAdjustment))
+                print("top contour points moved by "+str(stemAdjustment)+ " points")
+
+    glyph.performUndo()
+    
+yOPQ(110)
