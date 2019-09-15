13808ca06affe3c31cfe9fa54da6705a4cef66ae
Agyei <hello@agyei.design>
2019-09-14 20:06:34 -0400

    label adjustment sliders built, need to get in one window ffs

--- sources/scripts/pointshifterv3.py
+++ sources/scripts/pointshifterv3.py
@@ -1,31 +1,69 @@
+from vanilla import FloatingWindow, Slider
+from mojo.UI import Message
+
 f= CurrentFont()
-
-print
-
-from collections import OrderedDict
-
-# labelName = 'top'
-# g = CurrentGlyph()
-# g.prepareUndo()
-# for c in g:
-#     for b in c.bPoints:
-#         if labelName in b.naked().labels:
-#             b.move((0, 200))
-# g.performUndo()
-
+g=CurrentGlyph()
 
 def BuildLabelsList(theFont):
     Plabels=[]    
     for glyph in theFont:
         for contour in glyph:
                 for point in contour.points:
-                    # print(point.labels)
                     b=point.labels
-                if b not in Plabels:
-                    Plabels=Plabels+b
-                    Plabels=list(dict.fromkeys(Plabels)) #remove duplicates
+                Plabels=Plabels+b
+                Plabels=list(dict.fromkeys(Plabels)) #remove duplicates
     print(Plabels)
     return Plabels
+
+class MoveGlyphWindow:
+
+    '''Move the current glyph using sliders.'''
+
+    def __init__(self, glyph):
+
+        # if glyph is None, show a message
+        # store the glyph and initial move values as object attributes
+                    
+            self.g = glyph
+            self.moveX = 0
+
+            # create a floating window
+            self.w = FloatingWindow((200, 74), "move %s")
+
+            # add a slider for moving in the x axis
+            for labelslider in BuildLabelsList(f):
+                self.label= labelslider
+                self.w.labelslider = Slider(
+                        (10, 10, -10, 22),
+                        value=0, maxValue=100, minValue=-100,
+                        callback=self.adjust)
+
+            # open the window
+            self.w.open()
+
+    def adjust(self, sender):
+
+        # get the current x and y values from the sliders
+        valueX = self.w.sliderX.get()
+
+        # calculate actual move distance
+        x = self.moveX - valueX
+
+        # move the glyph
+        self.g.moveBy((x, 0))
+
+        # update saved values
+        self.moveX = valueX
+
+        # print the current move distance
+        print(self.moveX, self.moveY)
+
+BuildLabelsList(f)
+OpenWindow(MoveGlyphWindow, CurrentGlyph())
+
+
+
+
 
 
                     
