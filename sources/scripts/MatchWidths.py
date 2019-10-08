72888e39661c12f81d4c908ba66c8aa96b8cf034
Agyei <hello@agyei.design>
2019-10-08 17:58:18 -0400

    edited DS file and grade match script. neeed -1 edits on MinGrade

--- sources/scripts/MatchWidths.py
+++ sources/scripts/MatchWidths.py
@@ -1,10 +1,12 @@
-maxMaster=OpenFont('/Users/aamacbook/Work Interim/Krspy-VF/sources/drawing/krspy v2/instances/NarrowBlackMaxGrade copy.ufo')
-minMaster=OpenFont('/Users/aamacbook/Work Interim/Krspy-VF/sources/drawing/krspy v2/instances/NarrowBlackMinGrade copy.ufo')
+maxMaster=OpenFont('/Users/aamacbook/Work Interim/Krspy-VF/sources/drawing/krspy v2/instances/KRSPY-parametric-NarrowThinMaxGrade.ufo')
+minMaster=OpenFont('/Users/aamacbook/Work Interim/Krspy-VF/sources/drawing/krspy v2/instances/KRSPY-parametric_NarrowThinMinGrade.ufo')
 
 for k in maxMaster.keys():
     if maxMaster[k]:
-        # widthDiff=maxMaster[k].width-minMaster[k].width
-        # minMaster[k].leftMargin+=widthDiff/2
-        # minMaster[k].rightMargin+=widthDiff/2
+        maxMaster[k].leftMargin=int(maxMaster[k].leftMargin)
+        maxMaster[k].rightMargin=int(maxMaster[k].rightMargin)
+        widthDiff=maxMaster[k].width-minMaster[k].width
+        minMaster[k].leftMargin+=int(widthDiff/2)
+        minMaster[k].rightMargin+=int(widthDiff/2)
         print(minMaster[k].width)
         print(maxMaster[k].width)
