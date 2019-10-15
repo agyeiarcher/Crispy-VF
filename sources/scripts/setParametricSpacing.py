ab72897421a091e3eb5879402718de970eb5b1da
Agyei <hello@agyei.design>
2019-10-15 18:24:23 -0400

    glyph subs, spacing script edits, master tweaks

--- sources/scripts/setParametricSpacing.py
+++ sources/scripts/setParametricSpacing.py
@@ -7,13 +7,13 @@ styleName = f.info.styleName
 #making batches of letter groups below, to check for different things. The xCheckGroup lists are flattened to check if any characters are duplicated, so spacing isn't applied more than once to any one character. If they are, they're skipped over and reported
 
 rightSideNotch = ["O", "B", "K", "D", "R", "W", "V", "X"]
-leftSideNotch = ["A", "U", "X","O", "S.wide", "G.wide", "G"]
+leftSideNotch = ["A", "U", "X","O", "S.wide", "G.wide", "G", "C", "B"]
 
-rightSideFlat = ["J", "Q", "M", "N", "U", "H", "A", "N.wide", "I.wide"]
-leftSideFlat = ["M", "N", "K", "B", "E", "F", "P", "R", "W", "V", "L", "H", "E.wide", "F.wide", "N.wide", "I.wide"]
+rightSideFlat = ["J", "Q", "M", "N", "U", "H", "A", "N.wide", "I.narrow"]
+leftSideFlat = ["M", "N", "K", "B", "E", "F", "P", "R", "W", "V", "L", "H", "E.wide", "F.wide", "N.wide", "I.narrow"]
 
-rightSideSpecial = ["Z", "P", "T", "Y", "L", "Z.wide", "C.wide", "S.wide", "E.wide", "E", "F", "F.wide", "S"]
-leftSideSpecial = ["J", "Z", "Y", "T", "Z.wide", "S"]
+rightSideSpecial = ["Z", "P", "T", "Y", "L", "Z.wide", "C.wide", "S.wide", "E.wide", "E", "F", "F.wide", "S", "I", "C"]
+leftSideSpecial = ["J", "Z", "Y", "Z.wide", "S", "T", "I"]
 
 
 rightGroup = [rightSideNotch, rightSideSpecial, rightSideFlat]
@@ -44,51 +44,50 @@ def allClear(rightSideCheckGroup, leftSideCheckGroup):
 #we just need to cross-ref this before running the script at all. if there's a duplicate, fix it before we get started. doh fuck up.
     
 def setSpacing(f):
-    
-    if styleName == "x0y0t1000":
-        rightSideNotchValue = 185
-        leftSideNotchValue = 185        
-        rightSideFlatValue = 240
-        leftSideFlatValue = 240
-        rightSideSpecialValue = 85
-        leftSideSpecialValue  = 85
-    elif styleName == "x0y1000t1000":
-        rightSideNotchValue = 185
-        leftSideNotchValue = 185        
-        rightSideFlatValue = 240
-        leftSideFlatValue = 240
-        rightSideSpecialValue = 85
-        leftSideSpecialValue  = 85
-    elif styleName == "x1000y0t1000":
-        rightSideNotchValue = 365
-        leftSideNotchValue = 365        
-        rightSideFlatValue = 447
-        leftSideFlatValue = 447
-        rightSideSpecialValue = 123
-        leftSideSpecialValue  = 123
-    elif styleName == "x0y0t0":
-        rightSideNotchValue = 27
-        leftSideNotchValue = 27        
-        rightSideFlatValue = 31
-        leftSideFlatValue = 31
-        rightSideSpecialValue = 25
-        leftSideSpecialValue  = 25
-    elif styleName ==  "x0y1000t0":
-        rightSideNotchValue = 27
-        leftSideNotchValue = 27        
-        rightSideFlatValue = 31
-        leftSideFlatValue = 31
-        rightSideSpecialValue = 25
-        leftSideSpecialValue  = 25
-    elif styleName == "x1000y0t0":
-        rightSideNotchValue = 45
-        leftSideNotchValue = 45        
-        rightSideFlatValue = 66
-        leftSideFlatValue = 66
-        rightSideSpecialValue = 30
-        leftSideSpecialValue  = 30
-    
     if allClear(rightSideCheckGroup, leftSideCheckGroup):
+        if styleName == "x0y0t1000":
+            rightSideNotchValue = 185
+            leftSideNotchValue = 185        
+            rightSideFlatValue = 240
+            leftSideFlatValue = 240
+            rightSideSpecialValue = 85
+            leftSideSpecialValue  = 85
+        if styleName == "x0y1000t1000":
+            rightSideNotchValue = 85+40  
+            leftSideNotchValue = 85+40         
+            rightSideFlatValue = 125+40
+            leftSideFlatValue = 125+40
+            rightSideSpecialValue = 70+40
+            leftSideSpecialValue  = 70+40
+        if styleName == "x1000y0t1000":
+            rightSideNotchValue = 85+40  
+            leftSideNotchValue = 85+40         
+            rightSideFlatValue = 125+40
+            leftSideFlatValue = 125+40
+            rightSideSpecialValue = 70+40
+            leftSideSpecialValue  = 70+40
+        if styleName == "x0y0t0":
+            rightSideNotchValue = 29
+            leftSideNotchValue = 29        
+            rightSideFlatValue = 34
+            leftSideFlatValue = 34
+            rightSideSpecialValue = 27
+            leftSideSpecialValue  = 27
+        if styleName ==  "x0y1000t0":
+            rightSideNotchValue = 27
+            leftSideNotchValue = 27        
+            rightSideFlatValue = 31
+            leftSideFlatValue = 31
+            rightSideSpecialValue = 25
+            leftSideSpecialValue  = 25
+        if styleName == "x1000y0t0":
+            rightSideNotchValue = 45
+            leftSideNotchValue = 45        
+            rightSideFlatValue = 66
+            leftSideFlatValue = 66
+            rightSideSpecialValue = 30
+            leftSideSpecialValue  = 30
+
         for g in f.keys():
             #set right margins
             f[g].prepareUndo()
@@ -98,7 +97,7 @@ def setSpacing(f):
                 f[g].rightMargin =  rightSideFlatValue
             if f[g].name in rightSideSpecial:
                 f[g].rightMargin =  rightSideSpecialValue
-            
+    
             if f[g].name in leftSideNotch:
                 f[g].leftMargin =  leftSideNotchValue
             if f[g].name in leftSideFlat:
