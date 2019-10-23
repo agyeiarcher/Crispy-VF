857100207a86cd92adbb29eb5015f85d984f2ade
Agyei <hello@agyei.design>
2019-10-23 00:22:45 -0400

    grade builder script completed pew pew

---
+++ sources/designspaces/WEIGHTWIDTHGRADE/matchGrades.py
@@ -0,0 +1,43 @@
+import os, fnmatch
+
+minGradeFlag = "MinGrade"
+maxGradeFlag = "MaxGrade"
+
+minGradeMasters = []
+maxGradeMasters = []
+allMasters = []
+
+listOfFiles = os.listdir('.')
+pattern = "*.ufo"
+for entry in listOfFiles:
+    if fnmatch.fnmatch(entry, pattern):
+        if minGradeFlag in entry:
+            minGradeMasters.append(entry)
+        if maxGradeFlag in entry:
+            maxGradeMasters.append(entry)
+        allMasters.append(entry)
+
+def makeGrades(maxMaster, minMaster):
+    for k in maxMaster.keys():
+        if maxMaster[k]:
+            maxMaster[k].leftMargin=int(maxMaster[k].leftMargin)
+            maxMaster[k].rightMargin=int(maxMaster[k].rightMargin)
+            widthDiff=maxMaster[k].width-minMaster[k].width
+            if widthDiff%2 == 0:
+                minMaster[k].leftMargin+=int(widthDiff/2)
+                minMaster[k].rightMargin+=int(widthDiff/2)
+            else:
+                widthDiff+=1
+                minMaster[k].leftMargin+=int(widthDiff/2)-1
+                minMaster[k].rightMargin+=int(widthDiff/2)
+            minMaster.save()
+            maxMaster.save()
+            print(minMaster.info.styleName + " " + minMaster[k].name + " = " + str(minMaster[k].width))
+            print(maxMaster.info.styleName + " " + maxMaster[k].name + " = " + str(maxMaster[k].width)+"\n")
+
+for masterMax in maxGradeMasters:
+    for masterMin in minGradeMasters:
+        if masterMax.replace(maxGradeFlag, "") == masterMin.replace(minGradeFlag, ""):
+            maxMaster=OpenFont(masterMax, showInterface=False)
+            minMaster=OpenFont(masterMin, showInterface=False)
+            makeGrades(maxMaster, minMaster)
