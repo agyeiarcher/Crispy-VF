7b458c740da3fc1f5a6d29bb17fb06b003b85717
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2019-10-28 21:14:35 -0300

    setting up fontbakery automated QA checks (#33)
    * setting up fontbakery automated QA checks
    
    * preliminary work on dealing with com.google.fonts/check/name/license_url
    
    * run gftools to fix com.google.fonts/check/smart_dropout

---
+++ build.sh
@@ -0,0 +1,31 @@
+OUTPUT_DIR=build_output
+#SOURCE_DIR=source
+#
+rm $OUTPUT_DIR -rf
+mkdir $OUTPUT_DIR
+cp fonts/KRSPY-WEIGHWIDTHGRADE-XYT.ttf $OUTPUT_DIR
+cp ofl.txt $OUTPUT_DIR/OFL.txt
+#cp METADATA.pb $OUTPUT_DIR
+#cp DESCRIPTION.*.html $OUTPUT_DIR
+
+#for src in $SOURCE_DIR/*.glyphs
+#do
+#  fontmake -g $src -o ttf -i --output-dir $OUTPUT_DIR/
+#done
+
+for font in $OUTPUT_DIR/*.ttf
+do
+  gftools fix-nonhinting $font $font
+#  gftools fix-dsig $font --autofix
+done
+
+## Cleanup gftools mess:
+#rm $OUTPUT_DIR/*-backup-fonttools-prep-gasp.ttf
+
+export OPTIONS="--no-progress"
+export OPTIONS="$OPTIONS --exclude-checkid /check/ftxvalidator" # We lack this on Travis.
+export OPTIONS="$OPTIONS --exclude-checkid /check/metadata" # Comment this out after creating a METADATA.pb file.
+export OPTIONS="$OPTIONS --exclude-checkid /check/description" # Comment this out after creating a DESCRIPTION.en_us.html file.
+# export OPTIONS="$OPTIONS --exclude-checkid /check/varfont" # Comment this out when making a variable font.
+export OPTIONS="$OPTIONS --loglevel INFO --ghmarkdown Fontbakery-check-results.md"
+fontbakery check-googlefonts $OPTIONS $OUTPUT_DIR/*.ttf
