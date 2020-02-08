6cc90898bcedce67258c43b2734e57f3c31dac27
Agyei <hello@agyei.design>
2020-02-08 17:32:50 -0400

    adjusted waistlines on defaults, new B

--- sources/scripts/drawbot_gradetest1.py
+++ sources/scripts/drawbot_gradetest1.py
@@ -1,23 +1,29 @@
-nFrames = 5
+nFrames = 143
 variableFont = '../../font/variable_ttf/Crispy[GRAD,wdth,wght]-VF.ttf'
 margin = 20
 
-for i in range(1, nFrames):
+for i in range(1, nFrames+1):
     newPage(500,500)
     txt = FormattedString()
-    frameDuration = 1/24
-    gradeValue = -25    
+    frameDuration(1/24)
+    gradeValue = -25
+    weightValue = 1000
     phase = (2 * pi * i)/nFrames
     phaseMultiplier = (1 * sin(phase + (0.5 * pi)))
+    doublephaseMultiplier = (2 * sin(phase + (0.5 * pi)))
     # this shit right here is praxis. just use this for looping shit, use it everywhere
-    phasedGrade = gradeValue * phaseMultiplier
-    txt.append("TEST", font=variableFont , fontSize=220, lineHeight=height()-(margin*2),align="center", fill=(0, 0, 0), fontVariations = {'wght':1000, 'wdth':2, 'GRAD':phasedGrade})    
-    print(phasedGrade)
-    with savedState():
-        fill(1)
-        stroke(1,0,0)
-        rect(margin, margin, width()-(margin*2), height()-(margin*2))
-    textBox(txt, (margin, margin, width()-(margin*2), height()-(margin*2)))
+    phasedGrade = gradeValue * doublephaseMultiplier
+    phasedWeight = weightValue * phaseMultiplier
+    if phasedWeight > 1:
+        txt.append("FAIL", font=variableFont , fontSize=420, lineHeight=height()-(margin*2),align="center", fill=(1, 1, 1), fontVariations = {'wght':2, 'wdth':phasedWeight, 'GRAD':phasedGrade})    
+        # print(phasedGrade)
+        print(phasedGrade, phasedWeight, i)
+        with savedState():
+            fill(0)
+            stroke(1,0,0)
+        fill(0)
+        rect(0,0, width(), height())
+        textBox(txt, (margin, margin, width()-(margin*2), height()-(margin*2)))
 
 
 saveImage("gradetest.gif")
