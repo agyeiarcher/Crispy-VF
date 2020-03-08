c964191ce1e44efed301da676a829f9514ba2b75
Agyei <hello@agyei.design>
2020-03-08 11:06:33 -0400

    revising spacing

---
+++ sources/designspaces/SOURCE PARAMETRIC MASTERS - 9 MASTERS/generateUFOs.py
@@ -0,0 +1,3 @@
+import ufoProcessor
+ufoProcessor.build("/Users/aamacbook/Work Interim/Krspy-VF/sources/designspaces/SOURCE PARAMETRIC MASTERS/CRISPY-PARAMETRIC AXES.designspace")
+print("Instance builds confirmed!")
