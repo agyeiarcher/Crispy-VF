5dcb4877f4f5544ca9817b620d16f1e45ff25ad7
Agyei <hello@agyei.design>
2020-08-25 14:13:26 -0400

    updated proofing script with controls page

--- sources/documentation/proofs/Crispy Proof_Basic.py
+++ sources/documentation/proofs/Crispy Proof_Basic.py
@@ -1,19 +1,47 @@
 variableFontPath = "../../../font/variable_ttf/Crispy[srif,wdth,wght]-VF.ttf"
-fontList = listNamedInstances(variableFontPath).values()
-variationsList = list(listFontVariations(variableFontPath).keys())
-matrixList = list(listNamedInstances(variableFontPath).items())
-# print(matrixList)
-# for matrix in matrixList:
-#     print(matrix)
 
-# print(listNamedInstances(fontPath).values())
-# print(fontList)
-# for i in fontList:
-#     # variationsString = str(i).replace(":", "=").replace("{", "").replace("}", "").replace("'", "")
-#     # print(variationsString)
-#     font(fontPath)
-#     fontVariations(wght= 1.0, wdth= 15.0, SRIF= 1.0)
-#     text("HELLO Q", (100, 100))
+allCapSpacingString = "HHOHHOOHOO HHAHHOOAOO HHBHHOOBOO HHCHHOOCOO HHDHHOODOO HHEHHOOEOO HHFHHOOFOO HHGHHOOGOO HHIHHOOIOO HHJHHOOJOO HHKHHOOKOO HHLHHOOLOO HHMHHOOMOO HHNHHOONOO HHPHHOOPOO HHQHHOOQOO HHRHHOOROO HHSHHOOSOO HHTHHOOTOO HHUHHOOUOO HHVHHOOVOO HHWHHOOWOO HHXHHOOXOO HHYHHOOYOO HHZHHOOZOO"
+
+spacingStringBox = (40, 50, 690, 480)
+
+def calcTextSizeForBox(txt, box, minSize, maxSize, tolerance):
+    #calcTextSizeForBox function written originally by Just Van Rossum (thanks Just!)
+    # font needs to already have been set
+    #
+    # We're implementing a so called binary search here. We have a minimum value,
+    # a maximum value, and an initial value that we set right in the middle of
+    # min and max. The approach is then to see "is this value too high (there's
+    # an overflow)" or too low or maybe correct enough (no overflow).
+    # If there's an overflow, the maxValue will be set to the current value,
+    # and the value itsels is again placed right between min and max.
+    # If there's no overflow, the minValue is set to the current value.
+    # Soon, the value will be very close to the minSize value, and then
+    # we're done.
+    #
+    # This kind of algorithm is more efficient than just trying a lot of values
+    # in sequence, and can scale quite well.
+    #
+    fs = (minSize + maxSize) / 2
+    while True:
+        # Yes, this is potentially an endless loop, but
+        # we (try to) guarantee that it always terminates
+        fontSize(fs)
+        lineHeight(fs*1.3)
+        overflow = textOverflow(txt, box)
+        if overflow:
+            maxSize = fs
+            fs = (minSize + maxSize) / 2
+            if fs - minSize < tolerance:
+                fs = minSize
+                # giving up, there will be overflow
+                break
+        else:
+            if fs - minSize < tolerance:
+                # close enough, done
+                break
+            minSize = fs
+            fs = (minSize + maxSize) / 2
+    return fs
 
 def pageLabel(instanceName):
     font("")
@@ -22,14 +50,19 @@ def pageLabel(instanceName):
 for instanceName, setupMatrix in listNamedInstances(variableFontPath).items():
     print(instanceName, setupMatrix)
     newPage("LetterLandscape")
+    with savedState():
+        stroke(0)
+        fill()
+        rect(40, 40, width()-100, height()-130)    
     fontVariations(wght= setupMatrix['wght'])
     fontVariations(wdth = setupMatrix['wdth'])
     fontVariations(SRIF = setupMatrix['SRIF'])
     pageLabel(instanceName)
     font(variableFontPath)
-    text("HELLO Q", (40, 100))
-
-# print(list(listFontVariations(fontPath).keys()))
-# for variations in list(listFontVariations(fontPath).keys()):
-#     print(variations)
-
+    fs = calcTextSizeForBox(allCapSpacingString, spacingStringBox, minSize=5, maxSize=100, tolerance=0.1)
+    print(fs)
+    fontSize(fs)
+    lineHeight(fs*1.2)
+    spacingText = textBox(allCapSpacingString, spacingStringBox, align="left")
+    # newPage("LetterLandscape")
+    # print(fs)
